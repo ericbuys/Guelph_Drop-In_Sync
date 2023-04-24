a4b7a25c8374915f6efc0fcdc10523656aa3233c
ericbuys <eric.buys02@gmail.com>
2023-04-24 17:54:53 -0400

    Converted Scraping Code into an OOP Structure

---
+++ EventScraper.py
@@ -0,0 +1,72 @@
+from requests_html import HTMLSession
+from pyquery import PyQuery as pq
+from lxml import etree
+
+class Event:
+    def __init__(self, eventName, startTime, endTime, location, day):
+        self.eventName = eventName
+        self.startTime = startTime
+        self.endTime = endTime
+        self.location = location
+        self.day = day
+    
+    def __str__(self):
+        return ('-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n'
+                f'Event Name: {self.eventName}\n'
+                f'Location: {self.location}\n'
+                f'Start Time: {self.startTime}\n'
+                f'End Time: {self.endTime}\n'
+                f'Day: {self.day}')
+
+class EventScraper:
+    DAY_INDEX = {
+        'day_1': 'Sunday',
+        'day_2': 'Monday',
+        'day_3': 'Tuesday',
+        'day_4': 'Wednesday',
+        'day_5': 'Thursday',
+        'day_6': 'Friday',
+        'day_7': 'Saturday'
+    }
+
+    def __init__(self, url, targetEvent):
+        self.url = url
+        self.targetEvent = targetEvent
+        self.session = HTMLSession()
+        
+    def scrapeURL(self):
+        r = self.session.get(self.url)
+        r.html.render(wait=0.1, retries=8) #Fix: the num of retries shouldn't be hard-coded
+        scrapedEvents = []
+
+        displayTable = r.html.find("#AllEvents")[0]
+        eventBlocks = displayTable.find(".block.day")
+
+        for eventBlock in eventBlocks:
+            events = pq(etree.fromstring(eventBlock.html))
+            eventList = events('.event')
+            day = self.DAY_INDEX[eventBlock.attrs['class'][2]]
+
+            for event in eventList:
+                event = pq(event)
+                eventName = event('.eventName').text()
+
+                if(TARGET_EVENT == eventName):
+                    eventLocation = event('.eventLocation').text()
+                    startTime = event('.startTime').text()
+                    endTime = event('.endTime').text()
+                    
+                    newEvent = Event(eventName, startTime, endTime, eventLocation, day)
+                    scrapedEvents.append(newEvent)
+        
+        return scrapedEvents
+
+if __name__ == "__main__":
+    URL = "https://fitandrec.gryphons.ca/sports-clubs/drop-in-rec"
+    TARGET_EVENT = "Badminton"
+
+    scraper = EventScraper(URL, TARGET_EVENT)
+    scrapedEvents = scraper.scrapeURL()
+    
+    # for event in scrapedEvents:
+    #     print(event)
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # CalenderEventAdder
 
 ## References
-* [Setting up Gradle](https://spring.io/guides/gs/gradle/)
 * [Formatting a Readme](https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax)
